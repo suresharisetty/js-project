5d4c75d018bb29387f29047c5c33b2db32dced99
unknown <suresharisetty546@gmail.com>
2023-12-16 15:43:30 +0530

    added

---
+++ script.js
@@ -0,0 +1,37 @@
+const data = [
+    { name: "john", age: 24, profession: "admin" },
+    { name: "jane", age: 27, profession: "developer" },
+  ];
+  
+  // 1. Print Developers
+  function printDeveloper() {
+    const found = data.find((profession) =>  profession="developer");
+    console.log(found);
+  }
+  
+  // 2. Add Data
+  function addData() {}
+  
+  // 3. Remove Admins
+  function removeAdmin() {}
+  
+  // 4. Concatenate Array
+  function concatenateArray() {}
+  
+  // 5. Average Age
+  function averageAge() {}
+  
+  // 6. Age Check
+  function checkAgeAbove25() {}
+  
+  // 7. Unique Professions
+  function uniqueProfessions() {}
+  
+  // 8. Sort by Age
+  function sortByAge() {}
+  
+  // 9. Update Profession
+  function updateJohnsProfession() {}
+  
+  // 10. Profession Count
+  function getTotalProfessions() {}
